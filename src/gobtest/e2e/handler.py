ba646b37e1db95705193c6415cb30470e8bdef84
Jaap Koetsier <jaap@chariot-it.nl>
2020-07-08 16:55:12 +0200

    PGB-1555 Add latency in e2e tests to wait for imports/relates/dumps to be done. Fix unknown key errors in e2e tests

--- src/gobtest/e2e/handler.py
+++ src/gobtest/e2e/handler.py
@@ -13,17 +13,71 @@ def end_to_end_test_handler(msg):
     :param msg:
     :return:
     """
+    now = datetime.datetime.utcnow()
+    start_timestamp = int(now.replace(microsecond=0).timestamp())
+    header = msg.get('header', {})
+
+    # Set process id before call to logger.configure
+    header['process_id'] = header.get('process_id', f"{start_timestamp}.e2e_test")
+
     logger.configure(msg, 'E2E Test')
     logger.info("Clear any previous test data")
-    E2ETest().cleartests()
+
+    e2etest = E2ETest(header['process_id'])
+    e2etest.cleartests()
     logger.info("Start E2E Test")
+
+    return {
+        'header': {
+            **header,
+            'timestamp': now.isoformat(),
+            'workflow': e2etest.get_workflow(),
+        },
+        'contents': ''
+    }
+
+
+def end_to_end_execute_workflow_handler(msg):
+    logger.configure(msg, 'E2E Test')
+    workflow_to_execute = msg['header'].get('execute')
+    workflow_process_id = msg['header'].get('execute_process_id')
+    process_id = msg['header'].get('process_id')
+
+    assert all([workflow_to_execute, workflow_process_id, process_id]), \
+        "Expecting attributes 'execute', 'execute_process_id' and 'process_id' in header"
+
+    E2ETest(process_id).execute_workflow(workflow_to_execute, workflow_process_id)
+
     return {
         'header': {
             **msg.get('header', {}),
-            'timestamp': datetime.datetime.utcnow().isoformat(),
-            'workflow': E2ETest().get_workflow()
         },
-        'contents': ''
+        'summary': {
+            'warnings': logger.get_warnings(),
+            'errors': logger.get_errors(),
+        }
+    }
+
+
+def end_to_end_wait_handler(msg):
+    logger.configure(msg, 'E2E Test')
+    process_id = msg['header'].get('process_id')
+    wait_for_process_id = msg['header'].get('wait_for_process_id')
+    seconds = msg['header'].get('seconds')
+
+    assert all([process_id, wait_for_process_id, seconds]), \
+        "Expecting attributes 'process_id', 'wait_for_process_id' and 'seconds' in header"
+
+    E2ETest(process_id).wait(wait_for_process_id, seconds)
+
+    return {
+        'header': {
+            **msg.get('header', {}),
+        },
+        'summary': {
+            'warnings': logger.get_warnings(),
+            'errors': logger.get_errors(),
+        }
     }
 
 
@@ -33,12 +87,12 @@ def end_to_end_check_handler(msg):
     endpoint = msg['header'].get('endpoint')
     expect = msg['header'].get('expect')
     description = msg['header'].get('description')
+    process_id = msg['header'].get('process_id')
 
-    assert all([endpoint, expect, description]), \
-        "Expecting attributes 'endpoint', 'expect' and 'description' in header"
+    assert all([endpoint, expect, description, process_id]), \
+        "Expecting attributes 'endpoint', 'expect', 'description' and 'process_id' in header"
 
-    E2ETest().check(endpoint, expect, description)
-
+    E2ETest(process_id).check(endpoint, expect, description)
     return {
         'header': {
             **msg.get('header', {}),
--- src/tests/e2e/test_e2etest.py
+++ src/tests/e2e/test_e2etest.py
@@ -1,15 +1,23 @@
 from unittest import TestCase
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch, MagicMock, call
 
 import os
-from gobtest.e2e.e2etest import E2ETest, IMPORT, RELATE, END_TO_END_CHECK
+from gobtest.e2e.e2etest import E2ETest, IMPORT, RELATE, END_TO_END_CHECK, END_TO_END_EXECUTE, END_TO_END_WAIT
 
 
 @patch("gobtest.e2e.e2etest.logger", MagicMock())
 class TestE2Test(TestCase):
 
+    def test_init(self):
+        e2e = E2ETest('my process id')
+        self.assertEqual('my process id', e2e.process_id)
+
+        # Should not be possible to initialise without process id
+        with self.assertRaises(TypeError):
+            E2ETest()
+
     def test_remove_last_event(self):
-        e2e = E2ETest()
+        e2e = E2ETest('process_id')
         inp = "a;b;c\n1;2;3\n4;5;6\n"
         self.assertEqual(inp, e2e._remove_last_event(inp))
 
@@ -22,7 +30,7 @@ class TestE2Test(TestCase):
         expected_result = "B\nA\nC"
 
         mock_get.return_value = type('MockResponse', (object,), {'status_code': 200, 'text': api_result})
-        e2e = E2ETest()
+        e2e = E2ETest('process_id')
         e2e._load_testfile = MagicMock(return_value=expected_result)
         e2e._remove_last_event = MagicMock(side_effect=lambda x: x)
         e2e.api_base = 'API_BASE'
@@ -38,7 +46,7 @@ class TestE2Test(TestCase):
         expected_result = "B\nA\nC"
 
         mock_get.return_value = type('MockResponse', (object,), {'status_code': 500, 'text': api_result})
-        e2e = E2ETest()
+        e2e = E2ETest('process_id')
         e2e._load_testfile = MagicMock(return_value=expected_result)
         e2e._remove_last_event = MagicMock(side_effect=lambda x: x)
 
@@ -50,7 +58,7 @@ class TestE2Test(TestCase):
         expected_result = "B\nA\nD"
 
         mock_get.return_value = type('MockResponse', (object,), {'status_code': 200, 'text': api_result})
-        e2e = E2ETest()
+        e2e = E2ETest('process_id')
         e2e._load_testfile = MagicMock(return_value=expected_result)
         e2e._remove_last_event = MagicMock(side_effect=lambda x: x)
 
@@ -58,7 +66,7 @@ class TestE2Test(TestCase):
 
     @patch("builtins.open")
     def test_load_testfile(self, mock_open):
-        e2e = E2ETest()
+        e2e = E2ETest('process_id')
 
         self.assertEqual(mock_open.return_value.__enter__.return_value.read.return_value,
                          e2e._load_testfile('filename'))
@@ -74,7 +82,17 @@ class TestE2Test(TestCase):
                 'collection': 'col',
                 'application': 'app',
             }
-        }, E2ETest()._import_workflow_definition('cat', 'col', 'app'))
+        }, E2ETest('process_id')._import_workflow_definition('cat', 'col', 'app'))
+
+    def test_wait_step_workflow_definition(self):
+        self.assertEqual({
+            'type': 'workflow_step',
+            'step_name': END_TO_END_WAIT,
+            'header': {
+                'wait_for_process_id': 'wait process id',
+                'seconds': 248024,
+            }
+        }, E2ETest('process_id')._wait_step_workflow_definition('wait process id', 248024))
 
     def test_check_workflow_step_definition(self):
         self.assertEqual({
@@ -85,7 +103,17 @@ class TestE2Test(TestCase):
                 'expect': 'exp',
                 'description': 'desc',
             }
-        }, E2ETest()._check_workflow_step_definition('endp', 'exp', 'desc'))
+        }, E2ETest('process_id')._check_workflow_step_definition('endp', 'exp', 'desc'))
+
+    def test_execute_start_workflow_definition(self):
+        self.assertEqual({
+            'type': 'workflow_step',
+            'step_name': END_TO_END_EXECUTE,
+            'header': {
+                'execute': ['the', 'workflow'],
+                'execute_process_id': 'the process id',
+            }
+        }, E2ETest('process_id')._execute_start_workflow_definition(['the', 'workflow'], 'the process id'))
 
     def test_relate_workflow_definition(self):
         self.assertEqual({
@@ -96,48 +124,54 @@ class TestE2Test(TestCase):
                 'collection': 'col',
                 'attribute': 'attr'
             }
-        }, E2ETest()._relate_workflow_definition('cat', 'col', 'attr'))
+        }, E2ETest('process_id')._relate_workflow_definition('cat', 'col', 'attr'))
 
     def test_build_autoid_test_workflow(self):
-        e2e = E2ETest()
+        e2e = E2ETest('process_id')
         e2e.test_catalog = 'test cat'
         e2e.test_import_entity_autoid = 'autoid entity'
         e2e.test_import_autoid_sources = ['src A', 'src B']
         e2e.check_autoid_endpoint = 'autoid endpoint'
         e2e._import_workflow_definition = lambda *args: "import " + ",".join(args)
         e2e._check_workflow_step_definition = lambda *args: "check " + ",".join(args)
+        e2e._execute_start_workflow_definition = lambda workflow, process_id: (workflow, process_id)
+        e2e._wait_step_workflow_definition = lambda process_id, seconds=10: f"wait {seconds} for {process_id}"
 
         expect = [
-            'import test cat,autoid entity,src A',
+            (['import test cat,autoid entity,src A'], 'process_id.autoid.0'),
+            'wait 10 for process_id.autoid.0',
             'check autoid endpoint,src A,Import src A',
-            'import test cat,autoid entity,src B',
+            (['import test cat,autoid entity,src B'], 'process_id.autoid.1'),
+            'wait 10 for process_id.autoid.1',
             'check autoid endpoint,src B,Import src B'
         ]
         result = e2e._build_autoid_test_workflow()
-        print(result)
         self.assertEqual(result, expect)
 
     def test_build_autoid_states_test_workflow(self):
-        e2e = E2ETest()
+        e2e = E2ETest('process_id')
         e2e.test_catalog = 'test cat'
         e2e.test_import_entity_autoid_states = 'autoid entity'
         e2e.test_import_autoid_states_sources = ['src A', 'src B']
         e2e.check_autoid_states_endpoint = 'autoid endpoint'
         e2e._import_workflow_definition = lambda *args: "import " + ",".join(args)
         e2e._check_workflow_step_definition = lambda *args: "check " + ",".join(args)
+        e2e._execute_start_workflow_definition = lambda workflow, process_id: (workflow, process_id)
+        e2e._wait_step_workflow_definition = lambda process_id, seconds=10: f"wait {seconds} for {process_id}"
 
         expect = [
-            'import test cat,autoid entity,src A',
+            (['import test cat,autoid entity,src A'], 'process_id.autoid_states.0'),
+            'wait 10 for process_id.autoid_states.0',
             'check autoid endpoint,src A,Import src A',
-            'import test cat,autoid entity,src B',
+            (['import test cat,autoid entity,src B'], 'process_id.autoid_states.1'),
+            'wait 10 for process_id.autoid_states.1',
             'check autoid endpoint,src B,Import src B'
         ]
         result = e2e._build_autoid_states_test_workflow()
-        print(result)
         self.assertEqual(result, expect)
 
     def test_build_import_test_workflow(self):
-        e2e = E2ETest()
+        e2e = E2ETest('process_id')
         e2e.test_catalog = 'test cat'
         e2e.test_import_entity = 'import entity'
         e2e.test_import_sources = ['src A', 'src B']
@@ -145,22 +179,28 @@ class TestE2Test(TestCase):
         e2e._import_workflow_definition = lambda *args: "import " + ",".join(args)
         e2e._check_workflow_step_definition = lambda *args: "check " + ",".join(args)
         e2e._relate_workflow_definition = lambda *args: "relate " + ",".join(args)
+        e2e._execute_start_workflow_definition = lambda workflow, process_id: (workflow, process_id)
+        e2e._wait_step_workflow_definition = lambda process_id, seconds=10: f"wait {seconds} for {process_id}"
 
         expect = [
-            'import test cat,test_entity_ref,ADD',
-            'import test cat,test_entity_ref,MODIFY1',
-            "import test cat,import entity,src A",
-            "relate test cat,import entity,reference",
-            "check import endpoint,src A,Import src A",
-            "import test cat,import entity,src B",
-            "relate test cat,import entity,reference",
-            "check import endpoint,src B,Import src B",
+            (['import test cat,test_entity_ref,ADD',
+              'import test cat,test_entity_ref,MODIFY1',
+              'import test cat,import entity,src A',
+              'relate test cat,import entity,reference'],
+             'process_id.import_test.src A.0'),
+            'wait 10 for process_id.import_test.src A.0',
+            'check import endpoint,src A,Import src A',
+            (['import test cat,import entity,src B',
+              'relate test cat,import entity,reference'],
+             'process_id.import_test.src B.1'),
+            'wait 10 for process_id.import_test.src B.1',
+            'check import endpoint,src B,Import src B'
         ]
         result = e2e._build_import_test_workflow()
         self.assertEqual(result, expect)
 
     def test_build_relate_test_workflow(self):
-        e2e = E2ETest()
+        e2e = E2ETest('process_id')
         e2e.test_catalog = 'test cat'
         e2e.test_relation_entities = ['rel entity A', 'rel entity B']
         e2e.test_relation_src_entities = ['rel src A', 'rel src B']
@@ -170,36 +210,44 @@ class TestE2Test(TestCase):
         e2e._import_workflow_definition = lambda *args: "import " + ",".join(args)
         e2e._check_workflow_step_definition = lambda *args: "check " + ",".join(args)
         e2e._relate_workflow_definition = lambda *args: "relate " + ",".join(args)
+        e2e._execute_start_workflow_definition = lambda workflow, process_id: (workflow, process_id)
+        e2e._wait_step_workflow_definition = lambda process_id, seconds=10: f"wait {seconds} for {process_id}"
 
         self.assertEqual([
-            "import test cat,rel entity A,REL",
-            "import test cat,rel entity B,REL",
-            "relate test cat,rel src A,dst_a",
-            "check /dump/rel/tst_src A_tst_rel_dst_a/?format=csv,tst_src A_"
-            "tst_rel_dst_a,Relation tst_src A_tst_rel_dst_a",
-            "relate test cat,rel src A,dst_b",
-            "check /dump/rel/tst_src A_tst_rel_dst_b/?format=csv,tst_src A_"
-            "tst_rel_dst_b,Relation tst_src A_tst_rel_dst_b",
-            "relate test cat,rel src B,dst_a",
-            "check /dump/rel/tst_src B_tst_rel_dst_a/?format=csv,tst_src B_"
-            "tst_rel_dst_a,Relation tst_src B_tst_rel_dst_a",
-            "relate test cat,rel src B,dst_b",
-            "check /dump/rel/tst_src B_tst_rel_dst_b/?format=csv,tst_src B_"
-            "tst_rel_dst_b,Relation tst_src B_tst_rel_dst_b",
+            'import test cat,rel entity A,REL',
+            'import test cat,rel entity B,REL',
+            (['relate test cat,rel src A,dst_a'], 'process_id.relate.rel src A.rel_dst_a'),
+            'wait 10 for process_id.relate.rel src A.rel_dst_a',
+            'check /dump/rel/tst_src A_tst_rel_dst_a/?format=csv,tst_src '
+            'A_tst_rel_dst_a,Relation tst_src A_tst_rel_dst_a',
+            (['relate test cat,rel src A,dst_b'], 'process_id.relate.rel src A.rel_dst_b'),
+            'wait 10 for process_id.relate.rel src A.rel_dst_b',
+            'check /dump/rel/tst_src A_tst_rel_dst_b/?format=csv,tst_src '
+            'A_tst_rel_dst_b,Relation tst_src A_tst_rel_dst_b',
+            (['relate test cat,rel src B,dst_a'], 'process_id.relate.rel src B.rel_dst_a'),
+            'wait 10 for process_id.relate.rel src B.rel_dst_a',
+            'check /dump/rel/tst_src B_tst_rel_dst_a/?format=csv,tst_src '
+            'B_tst_rel_dst_a,Relation tst_src B_tst_rel_dst_a',
+            (['relate test cat,rel src B,dst_b'], 'process_id.relate.rel src B.rel_dst_b'),
+            'wait 10 for process_id.relate.rel src B.rel_dst_b',
+            'check /dump/rel/tst_src B_tst_rel_dst_b/?format=csv,tst_src '
+            'B_tst_rel_dst_b,Relation tst_src B_tst_rel_dst_b'
         ], e2e._build_relate_test_workflow())
 
     def test_build_relate_multiple_allowed_test_workflow(self):
-        e2e = E2ETest()
+        e2e = E2ETest('process_id')
         e2e.test_catalog = 'test cat'
         e2e._import_workflow_definition = lambda *args: "import " + ",".join(args)
         e2e._check_workflow_step_definition = lambda *args: "check " + ",".join(args)
         e2e._relate_workflow_definition = lambda *args: "relate " + ",".join(args)
+        e2e._execute_start_workflow_definition = lambda wf, process_id: 'execute_' + process_id
+        e2e._wait_step_workflow_definition = lambda process_id, seconds=10: f"wait {seconds} for {process_id}"
 
         # Test only for length. Is a long test.
-        self.assertEqual(63, len(e2e._build_relate_multiple_allowed_test_workflow()))
+        self.assertEqual(36, len(e2e._build_relate_multiple_allowed_test_workflow()))
 
     def test_build_e2e_workflow(self):
-        e2e = E2ETest()
+        e2e = E2ETest('process_id')
         e2e._build_autoid_test_workflow = MagicMock(return_value=['0', '1'])
         e2e._build_autoid_states_test_workflow = MagicMock(return_value=['2', '3'])
         e2e._build_import_test_workflow = MagicMock(return_value=['a', 'b'])
@@ -208,21 +256,43 @@ class TestE2Test(TestCase):
         self.assertEqual(['0', '1', '2', '3', 'a', 'b', 'c', 'd', 'e', 'f'], e2e._build_e2e_workflow())
 
     def test_get_workflow(self):
-        e2e = E2ETest()
+        e2e = E2ETest('process_id')
         e2e._build_e2e_workflow = MagicMock()
         self.assertEqual(e2e._build_e2e_workflow.return_value, e2e.get_workflow())
 
-    def test_check(self):
-        e2e = E2ETest()
-        e2e._check_api_output = MagicMock()
+    @patch("gobtest.e2e.e2etest.start_workflow")
+    def test_execute_workflow(self, mock_start_workflow):
+        e2e = E2ETest('process_id')
 
+        e2e.execute_workflow(['some', 'workflow'], 'some process id')
+
+        mock_start_workflow.assert_called_with(
+            {'workflow_name': 'dynamic'},
+            {'header': {
+                'workflow': ['some', 'workflow'],
+                'process_id': 'some process id',
+            }}
+        )
+
+    def test_check(self):
+        e2e = E2ETest('process_id')
+        e2e._check_api_output = MagicMock()
         e2e.check('a', 'b', 'c')
+
         e2e._check_api_output.assert_called_with('a', 'b', 'c')
+
+    @patch("gobtest.e2e.e2etest.time.sleep")
+    def test_wait(self, mock_sleep):
+        e2e = E2ETest('process_id')
+        e2e._log_info = MagicMock()
+
+        e2e.wait('some process id', 248)
+        mock_sleep.assert_called_with(248)
 
     @patch("gobtest.e2e.e2etest.requests.delete")
     def test_cleartests(self, mock_delete):
         mock_delete.return_value.status_code = 200
-        e2e = E2ETest()
+        e2e = E2ETest('process_id')
         e2e._log_error = MagicMock()
         e2e.cleartests()
         mock_delete.assert_called()
