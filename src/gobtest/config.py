fdb338c37afc5c18caf1559c2fa172aff700f8c1
Jaap Koetsier <jaap@chariot-it.nl>
2020-07-08 16:55:12 +0200

    PGB-1555 Add latency in e2e tests to wait for imports/relates/dumps to be done. Fix unknown key errors in e2e tests

--- src/gobtest/config.py
+++ src/gobtest/config.py
@@ -1,3 +1,5 @@
 import os
 
 API_HOST = os.getenv('API_HOST', 'http://localhost:8141')
+MANAGEMENT_API_HOST = os.getenv('MANAGEMENT_API_HOST', 'http://localhost:8143')
+MANAGEMENT_API_BASE = f"{MANAGEMENT_API_HOST}/gob_management"
