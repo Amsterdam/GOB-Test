7eef31533d0a4913c1580817442d401814b76741
Jasper Jager <mail@jasperjager.com>
2021-01-26 16:17:17 +0100

    Use public endpoints for state updates

--- src/gobtest/config.py
+++ src/gobtest/config.py
@@ -2,4 +2,4 @@ import os
 
 API_HOST = os.getenv('API_HOST', 'http://localhost:8141')
 MANAGEMENT_API_HOST = os.getenv('MANAGEMENT_API_HOST', 'http://localhost:8143')
-MANAGEMENT_API_BASE = f"{MANAGEMENT_API_HOST}/gob_management"
+MANAGEMENT_API_PUBLIC_BASE = f"{MANAGEMENT_API_HOST}/gob_management/public"
