99024c36ca9f96ecd4fa4af88604ba2c8176fdfd
Peter Adrichem <Peter.Adrichem@gmail.com>
2023-01-18 14:42:35 +0100

    Add standard code checks; updated Core and Config;

--- src/gobtest/config.py
+++ src/gobtest/config.py
@@ -1,5 +1,5 @@
 import os
 
-API_HOST = os.getenv('API_HOST', 'http://localhost:8141')
-MANAGEMENT_API_HOST = os.getenv('MANAGEMENT_API_HOST', 'http://localhost:8143')
+API_HOST = os.getenv("API_HOST", "http://localhost:8141")
+MANAGEMENT_API_HOST = os.getenv("MANAGEMENT_API_HOST", "http://localhost:8143")
 MANAGEMENT_API_PUBLIC_BASE = f"{MANAGEMENT_API_HOST}/gob_management/public"
--- src/test.sh
+++ src/test.sh
@@ -4,13 +4,38 @@ set -u # crash on missing env
 set -e # stop on any error
 
 # Coverage 6: coverage run --data-file=/tmp/.coveragerc …
-export COVERAGE_FILE=/tmp/.coverage
+export COVERAGE_FILE="/tmp/.coverage"
+
+# Uncomment files to pass through checks
+FILES=(
+#  "gobtest/data_consistency/handler.py"
+#  "gobtest/data_consistency/data_consistency_test.py"
+  "gobtest/data_consistency/__init__.py"
+  "gobtest/config.py"
+  "gobtest/__init__.py"
+#  "gobtest/e2e/handler.py"
+#  "gobtest/e2e/e2etest.py"
+  "gobtest/e2e/__init__.py"
+  "gobtest/e2e/expect/__init__.py"
+#  "gobtest/__main__.py"
+)
+
+echo "Running mypy"
+mypy "${FILES[@]}"
 
 echo "Running unit tests"
-coverage run --source=./gobtest -m pytest tests/
+coverage run --source=gobtest -m pytest
 
 echo "Coverage report"
-coverage report --show-missing --fail-under=100
+coverage report --fail-under=100
 
-echo "Running style checks"
-flake8 ./gobtest
+echo "Check if Black finds no potential reformat fixes"
+black --check --diff "${FILES[@]}"
+
+echo "Check for potential import sort"
+isort --check --diff "${FILES[@]}"
+
+echo "Running flake8 style checks"
+flake8 "${FILES[@]}"
+
+echo "Checks complete"
